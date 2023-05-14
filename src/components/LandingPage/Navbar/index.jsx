dd6b009e78a2a29f142074b63c93d39109db2264
Isabella Vilhaba <isabellavilhaba@gmail.com>
2023-05-14 00:47:44 -0300

    Estiliza navbar responsivo

--- src/components/LandingPage/Navbar/index.jsx
+++ src/components/LandingPage/Navbar/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { close, menu } from "../../../assets/LandingPage";
 import logo from "../../../assets/logo.png";
-import { ButtonLogin, ButtonRegister } from './styles'
+import { ButtonLogin, ButtonRegister } from "./styles";
 
 export default function Navbar() {
   const [active, setActive] = useState("Home");
@@ -31,12 +31,12 @@ export default function Navbar() {
       <img src={logo} alt="Phi4free" className="w-[124px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => (
+        {navLinks.map((nav) => (
           <li
             key={nav.id}
-            className={`font-normal cursor-pointer text-[16px] ${
+            className={`font-normal cursor-pointer text-[16px] mr-10 ${
               active === nav.title ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            }`}
             onClick={() => setActive(nav.title)}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
@@ -70,10 +70,22 @@ export default function Navbar() {
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            <li
+              className={`font-normal cursor-pointer text-[16px] mb-4 text-dimWhite`}
+              onClick={() => console.log("Aluno")}
+            >
+              Login
+            </li>
+            <li
+              className={`font-normal cursor-pointer text-[16px] mb-4 text-dimWhite`}
+              onClick={() => console.log("Professor")}
+            >
+              Cadastre-se
+            </li>
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
-                className={`font-medium cursor-pointer text-[16px] ${
+                className={`font-medium cursor-pointer text-[16px] mb-4 ${
                   active === nav.title ? "text-white" : "text-dimWhite"
                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
                 onClick={() => setActive(nav.title)}
