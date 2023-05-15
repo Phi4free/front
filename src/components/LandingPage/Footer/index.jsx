b95eabf7d5e050daac191aa20a681d0e96708b60
Isabella Vilhaba <isabellavilhaba@gmail.com>
2023-05-15 18:19:45 -0300

    Estiliza footer

---
+++ src/components/LandingPage/Footer/index.jsx
@@ -0,0 +1,115 @@
+import styles from "../../../styles/style";
+import logo from "../../../assets/logo.png";
+
+export default function Footer() {
+  const footerLinks = [
+    {
+      title: "Links Úteis",
+      links: [
+        {
+          name: "Quem Somos",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "Trabalhe Conosco",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "Termos de Uso",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "Política de Privacidade",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "Apoie o Projeto",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+      ],
+    },
+    {
+      title: "Nossos cursos",
+      links: [
+        {
+          name: "Educação Financeira",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "Educação Filosófica",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "Educação Física",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "Educação Funcional",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+      ],
+    },
+    {
+      title: "Fale conosco",
+      links: [
+        {
+          name: "E-mail",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+        {
+          name: "FAQ",
+          link: "https://phi4free.netlify.app/landing-page",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className="flex-[1] flex flex-col justify-start mr-10">
+          <img
+            src={logo}
+            alt="hoobank"
+            className="w-[266px] h-[72.14px] object-contain"
+          />
+          <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
+            Uma nova maneira de adquirir conhecimento de forma prática, fácil e
+            confiável.
+          </p>
+        </div>
+
+        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+          {footerLinks.map((footerlink) => (
+            <div
+              key={footerlink.title}
+              className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}
+            >
+              <h4 className="font-medium text-[18px] leading-[27px] text-white">
+                {footerlink.title}
+              </h4>
+              <ul className="list-none mt-4">
+                {footerlink.links.map((link, index) => (
+                  <li
+                    key={link.name}
+                    className={`font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
+                      index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                    }`}
+                  >
+                    {link.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+        <p className="font-normal text-center text-[18px] leading-[27px] text-white">
+          Copyright Ⓒ 2023 Phi4free. All Rights Reserved.
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,4 +1,16 @@
-import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox  } from "../assets/LandingPage";
+import {
+  people01,
+  people02,
+  people03,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+} from "../assets/LandingPage";
 
 export const feedback = [
   {
@@ -24,72 +36,6 @@ export const feedback = [
     name: "Kenn Gallagher",
     title: "Founder & Leader",
     img: people03,
-  },
-];
-
-export const footerLinks = [
-  {
-    title: "Useful Links",
-    links: [
-      {
-        name: "Content",
-        link: "https://www.hoobank.com/content/",
-      },
-      {
-        name: "How it Works",
-        link: "https://www.hoobank.com/how-it-works/",
-      },
-      {
-        name: "Create",
-        link: "https://www.hoobank.com/create/",
-      },
-      {
-        name: "Explore",
-        link: "https://www.hoobank.com/explore/",
-      },
-      {
-        name: "Terms & Services",
-        link: "https://www.hoobank.com/terms-and-services/",
-      },
-    ],
-  },
-  {
-    title: "Community",
-    links: [
-      {
-        name: "Help Center",
-        link: "https://www.hoobank.com/help-center/",
-      },
-      {
-        name: "Partners",
-        link: "https://www.hoobank.com/partners/",
-      },
-      {
-        name: "Suggestions",
-        link: "https://www.hoobank.com/suggestions/",
-      },
-      {
-        name: "Blog",
-        link: "https://www.hoobank.com/blog/",
-      },
-      {
-        name: "Newsletters",
-        link: "https://www.hoobank.com/newsletters/",
-      },
-    ],
-  },
-  {
-    title: "Partner",
-    links: [
-      {
-        name: "Our Partner",
-        link: "https://www.hoobank.com/our-partner/",
-      },
-      {
-        name: "Become a Partner",
-        link: "https://www.hoobank.com/become-a-partner/",
-      },
-    ],
   },
 ];
 
