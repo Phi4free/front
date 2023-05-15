1565fed11a740ae4f4832990c6179b6687fc6db9
BeatrizNataly <beatriznatyce@gmail.com>
2023-05-15 18:49:06 -0300

    init feature do popup esqueci a senha

---
+++ src/components/PopupSenha/styles.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.button `
+background-color: #FFC300;
+color: #000;
+width: 100%;
+margin-top: 6%;
+padding: 3%;
+font-size: 100%;
+font-weight: 600;
+cursor: pointer;
+transition: background-color 0.5s, color 0.5s;
+`
