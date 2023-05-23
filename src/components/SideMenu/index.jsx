97e84d2e9919006a0f1686799261375ac7dd8772
BeatrizNataly <beatriznatyce@gmail.com>
2023-05-23 18:20:38 -0300

    Correção de estilos

--- src/components/SideMenu/index.jsx
+++ src/components/SideMenu/index.jsx
@@ -1,7 +1,6 @@
 import { Fragment, useEffect, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useTranslation } from "react-i18next";
-import "./style.css";
 
 export default function SideMenu(props) {
     const { id, keepOnMenu, options } = props;
@@ -17,156 +16,23 @@ export default function SideMenu(props) {
 
                     <div className="w-full flex items-center gap-x-1.5 group select-none">
                         <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                            <div className="absolute top-0 left-0 w-full h-[102%] group-hover:translate-y-0 translate-y-0 bg-red-600 transition-all duration-300"></div>
-                        </div>
-                        <div
-                            className="bg-white/10 text-white group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
-                            href="#"
-                        >
-                            <svg
-                                className="h-5 w-5 !fill-red-500 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            ></svg>
-
-                            <span className="font-QuicksandMedium">Home</span>
-                        </div>
-                    </div>
-
-                    <div className="w-full flex items-center gap-x-1.5 group select-none">
-                        <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
                             <div className="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-red-600 transition-all duration-300"></div>
                         </div>
-                        <div
-                            className="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
-                            href="#"
-                        >
-                            <svg
-                                className="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600 transition-colors duration-200"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            >
-                                <g></g>
-                            </svg>
-                            <span className="font-QuicksandMedium">
-                                Discovery
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className="w-full flex items-center gap-x-1.5 group select-none">
-                        <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                            <div className="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-red-600 transition-all duration-300"></div>
-                        </div>
-                        <div
-                            className="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
-                            href="#"
-                        >
-                            <svg
-                                className="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            ></svg>
-
-                            <span className="font-QuicksandMedium">
-                                Friends
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className="w-full flex items-center gap-x-1.5 group select-none">
-                        <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                            <div className="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-red-600 transition-all duration-300"></div>
-                        </div>
-                        <div
-                            className="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
-                            href="#"
-                        >
-                            <svg
-                                className="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            ></svg>
-
-                            <span className="font-QuicksandMedium">
-                                Coming soon
-                            </span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="w-full pr-3 flex flex-col gap-y-1 text-gray-500 fill-gray-500 text-sm">
-                    <div className="font-QuicksandMedium pl-4 text-gray-400/60 text-xs text-[11px] uppercase">
-                        Profile
-                    </div>
-
-                    <div className="w-full flex items-center gap-x-1.5 group select-none">
-                        <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                            <div className="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-red-600 transition-all duration-300"></div>
-                        </div>
-                        <div
-                            className="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
-                            href="#"
-                        >
-                            <svg
-                                className="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            ></svg>
-                            <span className="font-QuicksandMedium">
-                                edit profile
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className="w-full flex items-center gap-x-1.5 group select-none">
-                        <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                            <div className="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-red-600 transition-all duration-300"></div>
-                        </div>
-                        <div
-                            className="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
-                            href="#"
-                        >
-                            <svg
-                                className="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            ></svg>
-                            <span className="font-QuicksandMedium">
-                                Settings
-                            </span>
-                        </div>
-                    </div>
-
-                    <div className="w-full flex items-center gap-x-1.5 group select-none">
-                        <div className="w-1 rounded-xl h-8 bg-transparent transition-colors duration-200 relative overflow-hidden">
-                            <div className="absolute top-0 left-0 w-full h-[102%] translate-y-full group-hover:translate-y-0 bg-red-600 transition-all duration-300"></div>
-                        </div>
-                        <div
-                            className="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
-                            href="#"
-                        >
-                            <svg
-                                className="h-5 w-5 group-hover:fill-red-600 dark:fill-gray-600  transition-colors duration-200"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                            ></svg>
-
-                            <span className="font-QuicksandMedium">
-                                log out
-                            </span>
-                        </div>
-                    </div>
-                    <div>
-                        <br />
-                        <br />
                         <button
-                            className="inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto bg-btnprimary"
-                            onClick={() => keepOnMenu(false)}
+                            className="group-hover:bg-white/10 w-full group-active:scale-95 self-stretch pl-2 rounded flex items-center space-x-2 transition-all duration-200 dark:group-hover:text-white dark:hover:text-white text-sm"
+                            onClick={() => console.log("Botao clicado")}
                         >
-                            FECHAR
+                            Discovery
                         </button>
                     </div>
+                </div>
+                <div>
+                    <button
+                        className="inline-flex w-full justify-center rounded-md py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto bg-btnprimary px-20"
+                        onClick={() => keepOnMenu(false)}
+                    >
+                        FECHAR
+                    </button>
                 </div>
             </aside>
         </>
--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -3,67 +3,76 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { LangSwitcher } from "../../components/LangSwitcher";
 import SideMenu from "../../components/SideMenu";
+import "./styles.css"
 //import {logoalt} from "../../assets/logoalt.png";
 
 export function Perfil() {
     // TO DO: Remember to translate do english all the static content from this page
     const { t } = useTranslation();
     const navigate = useNavigate();
-    const [showAdvancedMenu, isShowAdvancedMenu] = useState(false)
+    const [showAdvancedMenu, isShowAdvancedMenu] = useState(false);
 
     const opcoesAvancadas = {
         email: {
             titulo: "ALTERAR EMAIL",
             descricao: "Altere o email ao qual sua conta está associada",
-            acao: () => {console.log("handle: Alterar email")},
-         //   icon: logoalt,
+            acao: () => {
+                console.log("handle: Alterar email");
+            },
+            //   icon: logoalt,
         },
         senha: {
             titulo: "ALTERAR SENHA",
             descricao: "Altere a senha de acesso da sua conta",
-            acao: () => {console.log("handle: Alterar senha")},
-        //    icon: logoalt,
+            acao: () => {
+                console.log("handle: Alterar senha");
+            },
+            //    icon: logoalt,
         },
         segConta: {
             titulo: "SEGURANÇA & CONTA",
             descricao: "Opções de segurança da conta e plataforma",
-            acao: () => {console.log("handle: Segurança & Conta")},
-        //    icon: logoalt,
+            acao: () => {
+                console.log("handle: Segurança & Conta");
+            },
+            //    icon: logoalt,
         },
         logout: {
             titulo: "FAZER LOGOUT",
             descricao: "Desconecte a conta logada deste dispositivo",
-            acao: () => {console.log("handle: Fazer logout")},
-        //    icon: logoalt,
+            acao: () => {
+                console.log("handle: Fazer logout");
+            },
+            //    icon: logoalt,
         },
-
-    }
+    };
 
     return (
         <>
-        <br/>
-            <button
-                type="button"
-                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                onClick={() => isShowAdvancedMenu(true)}
-            >
-                Opções avançadas
-            </button>
-            <button
-                type="button"
-                className="inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto bg-btnprimary"
-                onClick={() => console.log("Editar perfil")}
-            >
-                Editar
-            </button>
-            {
-                showAdvancedMenu ?
-                <SideMenu 
-                id="advanced-menu"
-                options={opcoesAvancadas}
-                keepOnMenu={isShowAdvancedMenu}/>
-                : null
-            }
+            <br />
+            <div className="edit-btns">
+                <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => isShowAdvancedMenu(true)}
+                >
+                    Opções avançadas
+                </button>
+                <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm sm:ml-3 sm:w-auto bg-btnprimary"
+                    onClick={() => console.log("Editar perfil")}
+                >
+                    Editar
+                </button>
+            </div>
+            {showAdvancedMenu ? (
+                <SideMenu
+                    id="advanced-menu"
+                    options={opcoesAvancadas}
+                    keepOnMenu={isShowAdvancedMenu}
+                />
+            ) : null}
         </>
     );
 }
